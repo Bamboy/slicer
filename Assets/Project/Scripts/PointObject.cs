904d1a6cd1ba6a20da954b9d7d99f68fbbdd8ea3
adamfarnell <adamfarnell@hotmail.co.uk>
2016-02-19 22:51:39 +0000

    Put some shitty slicing sounds in to make sure the Wwise pipeline works.
    Soundbank is loaded onto UI Canvas and event is triggered in the PointObject script

--- Assets/Project/Scripts/PointObject.cs
+++ Assets/Project/Scripts/PointObject.cs
@@ -1,21 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PointObject : MonoBehaviour, ISlicable 
+public class PointObject : MonoBehaviour, ISlicable
 {
-	public int pointValue = 1;
+    public int pointValue = 1;
 
-	void ISlicable.OnSliced()
-	{
-		//Give us points
-		GameManager.Instance.AddPoints( pointValue );
-	}
+    void ISlicable.OnSliced()
+    {
+        //Give us points
+        GameManager.Instance.AddPoints(pointValue);
+        //Trigger "Slice" event in wwise
+        AkSoundEngine.PostEvent("Slice", GameObject.Find("UI Canvas"));
+    }
 
 
-	void OnCollisionEnter2D( Collision2D c )
-	{
-		if( c.collider.tag == "Destroy" )
-			Destroy( this.gameObject );
-	}
-
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        if (c.collider.tag == "Destroy")
+            Destroy(this.gameObject);
+    }
 }

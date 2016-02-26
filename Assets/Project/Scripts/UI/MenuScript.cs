24cbbe1f1aec7495891c3a22ea29b76c5af63074
Leland Lewis <ceolewis@yahoo.com>
2016-02-25 21:28:15 -0600

    Made an couple of Changes
    Okay, everything should remain the same.
    
    The Scripts are in the Folders that i've organized into and nothing from
    there is changed
    
    What i did:
    Added "Main Menu" and Menu script to the UI folder
    Adjusted the UI for Gameplay and add hearts to the screen.
    
    No major changes made and it should not be any issues there

---
+++ Assets/Project/Scripts/UI/MenuScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	public void Playgame()
+    {
+        Application.LoadLevel("object_spawing");
+    }
+    public void QuitGame()
+    {
+        //Not made yet but this is an Start
+    }
+}
